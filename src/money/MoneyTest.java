f98394f11839611e7efba173ff6fa79f806c81e4
fortissimo1997 <fortissimo1997@gmail.com>
2018-01-01 22:15:18 +0900

    TDD@p14

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -1,13 +1,16 @@
 package money;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
 	@Test
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);;
+		assertEquals(15, product.amount);
 	}
 }
