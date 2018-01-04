f66f7b4a1827fcc241295b273697311ac4574fee
fortissimo1997 <fortissimo1997@gmail.com>
2018-01-04 21:45:03 +0900

    TDD@p71
    - chapter 10 ended.

--- src/money/Money.java
+++ src/money/Money.java
@@ -7,7 +7,9 @@ class Money {
 		this.amount = amount;
 		this.currency = currency;
 	}
-	Money times(int multiplier) { return null; };
+	Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
 	String currency() {
 		return currency;
 	}
